69cf9145650654cfdb67ca3f885581e356768d6f
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2008-02-12 17:24:20 +0000

    Adding file

---
+++ trunk/src/figtree/treeviewer/ControllerOptionsPanel.java
@@ -0,0 +1,36 @@
+package figtree.treeviewer;
+
+import org.virion.jam.panels.OptionsPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Andrew Rambaut
+ * @version $Id$
+ */
+public class ControllerOptionsPanel extends OptionsPanel {
+
+	public ControllerOptionsPanel() {
+		super();
+	}
+
+	public ControllerOptionsPanel(int hGap, int vGap) {
+		super(hGap, vGap);
+	}
+
+	protected void adjustComponent(JComponent comp) {
+		// comp.putClientProperty("Quaqua.Component.visualMargin", new Insets(0,0,0,0));
+        Font font = UIManager.getFont("SmallSystemFont");
+        if (font != null) {
+            comp.setFont(font);
+        }
+        comp.putClientProperty("JComponent.sizeVariant", "small");
+		if (comp instanceof JButton) {
+			comp.putClientProperty("JButton.buttonType", "roundRect");
+		}
+		if (comp instanceof JComboBox) {
+			//comp.putClientProperty("JComboBox.isSquare", Boolean.TRUE);
+		}
+	}
+}
