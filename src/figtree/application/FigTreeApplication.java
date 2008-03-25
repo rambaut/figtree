5e43802e9746978e9ed7bbca606a8872870fff7f
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2008-03-25 16:12:46 +0000

    Adding command line interface

--- src/figtree/application/FigTreeApplication.java
+++ src/figtree/application/FigTreeApplication.java
@@ -16,6 +16,8 @@ import org.virion.jam.framework.*;
 import org.virion.jam.mac.Utils;
 import org.virion.jam.controlpalettes.BasicControlPalette;
 import org.virion.jam.controlpalettes.ControlPalette;
+import org.virion.jam.util.IconUtils;
+import org.virion.jam.app.Arguments;
 
 import java.awt.geom.Rectangle2D;
 import java.awt.*;
@@ -32,90 +34,90 @@ import javax.swing.*;
 
 public class FigTreeApplication extends MultiDocApplication {
 
-	public static FigTreeApplication application;
+    public static FigTreeApplication application;
 
-	public FigTreeApplication(MenuBarFactory menuBarFactory, String nameString, String aboutString, Icon icon,
-	                          String websiteURLString,
-	                          String helpURLString) {
-		super(menuBarFactory, nameString, aboutString, icon, websiteURLString, helpURLString);
+    public FigTreeApplication(MenuBarFactory menuBarFactory, String nameString, String aboutString, Icon icon,
+                              String websiteURLString,
+                              String helpURLString) {
+        super(menuBarFactory, nameString, aboutString, icon, websiteURLString, helpURLString);
 
-		addPreferencesSection(new GeneralPreferencesSection());
-		addPreferencesSection(new AppearancePreferencesSection());
-		addPreferencesSection(new AdvancedPreferencesSection());
-	}
+        addPreferencesSection(new GeneralPreferencesSection());
+        addPreferencesSection(new AppearancePreferencesSection());
+        addPreferencesSection(new AdvancedPreferencesSection());
+    }
 
-	public DocumentFrame doOpenFile(File file) {
-		DocumentFrame documentFrame = getUpperDocumentFrame();
-		if (documentFrame != null && documentFrame.getFile() == null) {
-			documentFrame.openFile(file);
-			return documentFrame;
-		} else {
-			return super.doOpenFile(file);
-		}
-	}
+    public DocumentFrame doOpenFile(File file) {
+        DocumentFrame documentFrame = getUpperDocumentFrame();
+        if (documentFrame != null && documentFrame.getFile() == null) {
+            documentFrame.openFile(file);
+            return documentFrame;
+        } else {
+            return super.doOpenFile(file);
+        }
+    }
 
-	public void doPaste() {
+    public void doPaste() {
 
-	}
+    }
 
-	static public void createPDF(String treeFileName, String pdfFileName) {
+    static public void createGraphic(String graphicFormat, String treeFileName, String graphicFileName) {
 
-		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(treeFileName));
-			String line = bufferedReader.readLine();
-			while (line != null && line.length() == 0) {
-				line = bufferedReader.readLine();
-			}
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(treeFileName));
+            String line = bufferedReader.readLine();
+            while (line != null && line.length() == 0) {
+                line = bufferedReader.readLine();
+            }
 
-			bufferedReader.close();
+            bufferedReader.close();
 
-			boolean isNexus = (line != null && line.toUpperCase().contains("#NEXUS"));
+            boolean isNexus = (line != null && line.toUpperCase().contains("#NEXUS"));
 
-			Reader reader = new FileReader(treeFileName);
+            Reader reader = new FileReader(treeFileName);
 
-			Map<String, Object> settings = new HashMap<String, Object>();
+            Map<String, Object> settings = new HashMap<String, Object>();
 
-			ExtendedTreeViewer treeViewer = new ExtendedTreeViewer();
-			ControlPalette controlPalette = new BasicControlPalette(200, BasicControlPalette.DisplayMode.ONLY_ONE_OPEN);
-			FigTreePanel figTreePanel = new FigTreePanel(null, treeViewer, controlPalette);
+            ExtendedTreeViewer treeViewer = new ExtendedTreeViewer();
+            ControlPalette controlPalette = new BasicControlPalette(200, BasicControlPalette.DisplayMode.ONLY_ONE_OPEN);
+            FigTreePanel figTreePanel = new FigTreePanel(null, treeViewer, controlPalette);
 
-			// First of all, fully populate the settings map so that
-			// all the settings have defaults
-			controlPalette.getSettings(settings);
+            // First of all, fully populate the settings map so that
+            // all the settings have defaults
+            controlPalette.getSettings(settings);
 
-			List<Tree> trees = new ArrayList<Tree>();
+            List<Tree> trees = new ArrayList<Tree>();
 
-			if (isNexus) {
-				FigTreeNexusImporter importer = new FigTreeNexusImporter(reader);
-				trees.add(importer.importNextTree());
+            if (isNexus) {
+                FigTreeNexusImporter importer = new FigTreeNexusImporter(reader);
+                trees.add(importer.importNextTree());
 
-				// Try to find a figtree block and if found, parse the settings
-				while (true) {
-					try {
-						importer.findNextBlock();
-						if (importer.getNextBlockName().equalsIgnoreCase("FIGTREE")) {
-							importer.parseFigTreeBlock(settings);
-						}
-					} catch (EOFException ex) {
-						break;
-					}
-				}
-			} else {
-				NewickImporter importer = new NewickImporter(reader, true);
-				trees.add(importer.importNextTree());
-			}
+                // Try to find a figtree block and if found, parse the settings
+                while (true) {
+                    try {
+                        importer.findNextBlock();
+                        if (importer.getNextBlockName().equalsIgnoreCase("FIGTREE")) {
+                            importer.parseFigTreeBlock(settings);
+                        }
+                    } catch (EOFException ex) {
+                        break;
+                    }
+                }
+            } else {
+                NewickImporter importer = new NewickImporter(reader, true);
+                trees.add(importer.importNextTree());
+            }
 
-			if (trees.size() == 0) {
-				throw new ImportException("This file contained no trees.");
-			}
+            if (trees.size() == 0) {
+                throw new ImportException("This file contained no trees.");
+            }
 
-			treeViewer.setTrees(trees);
+            treeViewer.setTrees(trees);
 
-			controlPalette.setSettings(settings);
+            controlPalette.setSettings(settings);
 
-			//Rectangle2D bounds = treeViewer.getContentPane().getBounds();
-			Rectangle2D bounds = new Rectangle2D.Double(0.0, 0.0, 800, 800);
-			treeViewer.getContentPane().setBounds(bounds.getBounds());
+            //Rectangle2D bounds = treeViewer.getContentPane().getBounds();
+            Rectangle2D bounds = new Rectangle2D.Double(0.0, 0.0, 800, 800);
+            treeViewer.getContentPane().setBounds(bounds.getBounds());
 //			Document document = new Document(new com.lowagie.text.Rectangle((float)bounds.getWidth(), (float)bounds.getHeight()));
 //
 //			// step 2
@@ -132,113 +134,175 @@ public class FigTreeApplication extends MultiDocApplication {
 //			cb.addTemplate(tp, 0, 0);
 //			document.close();
 
-		} catch(ImportException ie) {
-			throw new RuntimeException("Error writing PDF file: " + ie);
+        } catch(ImportException ie) {
+            throw new RuntimeException("Error writing PDF file: " + ie);
 //		} catch(DocumentException de) {
 //			throw new RuntimeException("Error writing PDF file: " + de);
-		} catch(IOException ioe) {
-			throw new RuntimeException("Error writing PDF file: " + ioe);
-		}
+        } catch(IOException ioe) {
+            throw new RuntimeException("Error writing PDF file: " + ioe);
+        }
 
-	}
+    }
 
-	// Main entry point
-	static public void main(String[] args) {
+    public static void centreLine(String line, int pageWidth) {
+        int n = pageWidth - line.length();
+        int n1 = n / 2;
+        for (int i = 0; i < n1; i++) { System.out.print(" "); }
+        System.out.println(line);
+    }
 
-		if (args.length == 2) {
-			// command line version...
-			createPDF(args[0], args[1]);
-			System.exit(0);
-		}
+    public static void printTitle() {
+        System.out.println();
+        centreLine("FigTree v1.1.3, 2006-2008", 60);
+        centreLine("Tree Figure Drawing Tool", 60);
+        centreLine("Andrew Rambaut", 60);
+        System.out.println();
+        centreLine("Institute of Evolutionary Biology", 60);
+        centreLine("University of Edinburgh", 60);
+        centreLine("a.rambaut@ed.ac.uk", 60);
+        System.out.println();
+        centreLine("http://tree.bio.ed.ac.uk/", 60);
+        centreLine("Uses the Java Evolutionary Biology Library (JEBL)", 60);
+        centreLine("http://jebl.sourceforge.net/", 60);
+        centreLine("Thanks to Alexei Drummond, Joseph Heled, Philippe Lemey, ", 60);
+        centreLine("Tulio de Oliveira & Beth Shapiro", 60);
+    }
 
-		boolean lafLoaded = false;
+    public static void printUsage(Arguments arguments) {
 
-		if (Utils.isMacOSX()) {
-			if (Utils.getMacOSXVersion().startsWith("10.5")) {
-				System.setProperty("apple.awt.brushMetalLook","true");
-			}
+        arguments.printUsage("figtree", "[<tree-file-name>] [<graphic-file-name>]");
+        System.out.println();
+        System.out.println("  Example: figtree test.tree");
+        System.out.println("  Example: figtree -graphic PDF test.tree test.pdf");
+        System.out.println();
+    }
 
-			System.setProperty("apple.laf.useScreenMenuBar","true");
-			System.setProperty("apple.awt.draggableWindowBackground","true");
-			System.setProperty("apple.awt.showGrowBox","true");
-			System.setProperty("apple.awt.graphics.UseQuartz","true");
+    // Main entry point
+    static public void main(String[] args) {
+
+        Arguments arguments = new Arguments(
+                new Arguments.Option[] {
+                        new Arguments.StringOption("graphic", new String[] {
+                                "PDF", "SVG", "WMF", "PNG", "GIF", "JPEG"
+                        }, false, "produce a graphic with the given format"),
+                        new Arguments.Option("help", "option to print this message")
+                });
+
+        try {
+            arguments.parseArguments(args);
+        } catch (Arguments.ArgumentException ae) {
+            System.out.println();
+            System.out.println(ae.getMessage());
+            System.out.println();
+            printTitle();
+            printUsage(arguments);
+            System.exit(1);
+        }
+
+        if (arguments.hasOption("help")) {
+            printTitle();
+            printUsage(arguments);
+            System.exit(0);
+        }
+
+        if (arguments.hasOption("graphic")) {
+            // command line version...
+            String graphicFormat = arguments.getStringOption("graphic");
+            String[] args2 = arguments.getLeftoverArguments();
+
+            printTitle();
+            createGraphic(graphicFormat, args2[0], args2[1]);
+            System.exit(0);
+        }
+
+        boolean lafLoaded = false;
+
+        if (Utils.isMacOSX()) {
+            if (Utils.getMacOSXVersion().startsWith("10.5")) {
+                System.setProperty("apple.awt.brushMetalLook","true");
+            }
+
+            System.setProperty("apple.laf.useScreenMenuBar","true");
+            System.setProperty("apple.awt.draggableWindowBackground","true");
+            System.setProperty("apple.awt.showGrowBox","true");
+            System.setProperty("apple.awt.graphics.UseQuartz","true");
 
 //			if (!Utils.getMacOSXVersion().startsWith("10.5")) {
-			// set the Quaqua Look and Feel in the UIManager
-			try {
-				//System.setProperty("Quaqua.Debug.showClipBounds","true");
-				//System.setProperty("Quaqua.Debug.showVisualBounds","true");
-				LookAndFeel lafClass;
+            // set the Quaqua Look and Feel in the UIManager
+            try {
+                //System.setProperty("Quaqua.Debug.showClipBounds","true");
+                //System.setProperty("Quaqua.Debug.showVisualBounds","true");
+                LookAndFeel lafClass;
 
-				if (Utils.getMacOSXVersion().startsWith("10.5")) {
-					lafClass = ch.randelshofer.quaqua.subset.Quaqua14ColorChooserLAF.class.newInstance();
-				} else {
-					lafClass = QuaquaLookAndFeel.class.newInstance();
-				}
+                if (Utils.getMacOSXVersion().startsWith("10.5")) {
+                    lafClass = ch.randelshofer.quaqua.subset.Quaqua14ColorChooserLAF.class.newInstance();
+                } else {
+                    lafClass = QuaquaLookAndFeel.class.newInstance();
+                }
 
-				UIManager.setLookAndFeel(lafClass);
+                UIManager.setLookAndFeel(lafClass);
 
-				lafLoaded = true;
-			} catch (Exception e) {
-			}
+                lafLoaded = true;
+            } catch (Exception e) {
+            }
 
-			UIManager.put("SystemFont", new Font("Lucida Grande", Font.PLAIN, 13));
-			UIManager.put("SmallSystemFont", new Font("Lucida Grande", Font.PLAIN, 11));
-		}
+            UIManager.put("SystemFont", new Font("Lucida Grande", Font.PLAIN, 13));
+            UIManager.put("SmallSystemFont", new Font("Lucida Grande", Font.PLAIN, 11));
+        }
 
-		if (!lafLoaded) {
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		}
+        if (!lafLoaded) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
 
-		java.net.URL url = FigTreeApplication.class.getResource("images/figtreeLogo.png");
-		Icon icon = null;
+        java.net.URL url = FigTreeApplication.class.getResource("images/figtreeLogo.png");
+        Icon icon = null;
 
-		if (url != null) {
-			icon = new ImageIcon(url);
-		}
+        if (url != null) {
+            icon = new ImageIcon(url);
+        }
 
-		final String nameString = "FigTree";
-		final String versionString = "1.1.3";
-		String aboutString = "<html><center>Tree Figure Drawing Tool<br>Version " + versionString + "<br>2006-2008, Andrew Rambaut<br>" +
-				"Institute of Evolutionary Biology, University of Edinburgh.<br><br>" +
-				"<a href=\"http://tree.bio.ed.ac.uk/\">http://tree.bio.ed.ac.uk/</a><br><br>" +
-				"Uses the Java Evolutionary Biology Library (JEBL)<br>" +
-				"<a href=\"http://sourceforge.net/projects/jebl/\">http://jebl.sourceforge.net/</a><br><br>" +
-				"Thanks to Alexei Drummond, Joseph Heled, Philippe Lemey, <br>Tulio de Oliveira & Beth Shapiro</center></html>";
+        final String nameString = "FigTree";
+        final String versionString = "1.1.3";
+        String aboutString = "<html><center>Tree Figure Drawing Tool<br>Version " + versionString + "<br>2006-2008, Andrew Rambaut<br>" +
+                "Institute of Evolutionary Biology, University of Edinburgh.<br><br>" +
+                "<a href=\"http://tree.bio.ed.ac.uk/\">http://tree.bio.ed.ac.uk/</a><br><br>" +
+                "Uses the Java Evolutionary Biology Library (JEBL)<br>" +
+                "<a href=\"http://sourceforge.net/projects/jebl/\">http://jebl.sourceforge.net/</a><br><br>" +
+                "Thanks to Alexei Drummond, Joseph Heled, Philippe Lemey, <br>Tulio de Oliveira & Beth Shapiro</center></html>";
 
-		String websiteURLString = "http://tree.bio.ed.ac.uk/software/figtree/";
-		String helpURLString = "http://tree.bio.ed.ac.uk/software/figtree/";
+        String websiteURLString = "http://tree.bio.ed.ac.uk/software/figtree/";
+        String helpURLString = "http://tree.bio.ed.ac.uk/software/figtree/";
 
-		FigTreeApplication.application = new FigTreeApplication(new FigTreeMenuBarFactory(), nameString, aboutString, icon,
-				websiteURLString, helpURLString);
+        FigTreeApplication.application = new FigTreeApplication(new FigTreeMenuBarFactory(), nameString, aboutString, icon,
+                websiteURLString, helpURLString);
 
-		application.setDocumentFrameFactory(new DocumentFrameFactory() {
-			public DocumentFrame createDocumentFrame(Application app, MenuBarFactory menuBarFactory) {
-				return new FigTreeFrame(nameString + " v" + versionString);
-			}
-		});
+        application.setDocumentFrameFactory(new DocumentFrameFactory() {
+            public DocumentFrame createDocumentFrame(Application app, MenuBarFactory menuBarFactory) {
+                return new FigTreeFrame(nameString + " v" + versionString);
+            }
+        });
 
-		application.initialize();
+        application.initialize();
 
-		if (args.length > 0) {
-			for (String arg : args) {
-				application.doOpen(arg);
-			}
-		}
+        if (args.length > 0) {
+            for (String arg : args) {
+                application.doOpen(arg);
+            }
+        }
 
 //		if (!org.virion.jam.mac.Utils.isMacOSX() && application.getUpperDocumentFrame() == null) {
 //			// If we haven't opened any files by now, prompt for one...
 //			application.doOpen();
 //		}
 
-		if (application.getUpperDocumentFrame() == null) {
-			// If we haven't opened any files by now, open a blank window...
-			application.doNew();
-		}
-	}
+        if (application.getUpperDocumentFrame() == null) {
+            // If we haven't opened any files by now, open a blank window...
+            application.doNew();
+        }
+    }
 
 }
