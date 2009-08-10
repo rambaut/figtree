c28fefad0d4c827ce180aeb267700e7a83f62648
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2009-08-10 17:16:35 +0000

    Last tweaks to v1.2.3

--- src/figtree/treeviewer/treelayouts/RectilinearTreeLayout.java
+++ src/figtree/treeviewer/treelayouts/RectilinearTreeLayout.java
@@ -69,8 +69,11 @@ public class RectilinearTreeLayout extends AbstractTreeLayout {
 
 	public Shape getHeightArea(double height1, double height2) {
 		double x = height1;
+        if (isAxisReversed()) {
+            x = maxXPosition - x;
+        }
 		double y = 0.0;
-		double w = height2 - height1;
+		double w = Math.abs(height2 - height1);
 		double h = 1.0;
 		return new Rectangle2D.Double(x, y, w, h);
 	}
