16109b9b6d2b868afeb2717e05b07838d89905b6
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2012-09-12 09:08:17 +0000

    More node shape work...

--- src/figtree/treeviewer/TreePane.java
+++ src/figtree/treeviewer/TreePane.java
@@ -1402,26 +1402,19 @@ public class TreePane extends JComponent implements PainterListener, Printable {
         // Paint node shapes
         if (nodeShapePainter != null && nodeShapePainter.isVisible()) {
             // paint the tip shapes...
-            for (Node node : nodeShapeTransforms.keySet()) {
-                AffineTransform tipLabelTransform = tipLabelTransforms.get(node);
-                g2.transform(tipLabelTransform);
-                nodeShapePainter.paint(g2, node, new Rectangle2D.Double(0.0, 0.0, 1, 1));
-
-                g2.setTransform(oldTransform);
-            }
-
-            for (Node node : nodeLabelTransforms.keySet()) {
-                AffineTransform nodeLabelTransform = nodeLabelTransforms.get(node);
-                g2.transform(nodeLabelTransform);
-                nodeShapePainter.paint(g2, node, new Rectangle2D.Double(0.0, 0.0, 1, 1));
-
-                g2.setTransform(oldTransform);
-            }
-
-//            for (Node node : nodeShapes.keySet() ) {
-//                Point2D nodePoint = node.get(node);
-//                nodeShape = translate.createTransformedShape(nodeShape);
+//            for (Node node : nodeShapeTransforms.keySet()) {
+//                AffineTransform nodeShapeTransform = nodeShapeTransforms.get(node);
+//                g2.transform(nodeShapeTransform);
+//                nodeShapePainter.paint(g2, node, new Rectangle2D.Double(0.0, 0.0, 1, 1));
+//
+//                g2.setTransform(oldTransform);
 //            }
+
+            for (Node node : nodeShapes.keySet() ) {
+                Shape nodeShape = nodeShapes.get(node);
+                nodeShape = transform.createTransformedShape(nodeShape);
+                nodeShapePainter.paint(g2, node, nodeShape);
+            }
         }
 
         // Paint tip labels
