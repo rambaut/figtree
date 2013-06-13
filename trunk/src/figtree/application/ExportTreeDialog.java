1a1f44ae0136d46e7479ff21ede82a8df3b9b5bb
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2013-06-13 15:57:27 +0000

    Implementing a JSON based tree export format.

--- trunk/src/figtree/application/ExportTreeDialog.java
+++ trunk/src/figtree/application/ExportTreeDialog.java
@@ -35,101 +35,101 @@ import java.awt.event.ItemEvent;
  */
 public class ExportTreeDialog {
 
-	public enum Format {
-		NEXUS("NEXUS"),
-		NEWICK("Newick");
+    public enum Format {
+        NEXUS("NEXUS"),
+        NEWICK("Newick"),
+        JSON("JSON");
 
-		Format(String name) {
-			this.name = name;
-		}
+        Format(String name) {
+            this.name = name;
+        }
 
-		public String toString() {
-			return name;
-		}
+        public String toString() {
+            return name;
+        }
 
-		private String name;
-	}
+        private String name;
+    }
 
-	private final JDialog dialog;
-	private final JOptionPane optionPane;
+    private final JDialog dialog;
+    private final JOptionPane optionPane;
 
-	private JComboBox formatCombo = null;
-	private JCheckBox asDisplayedCheck = new JCheckBox("Save as currently displayed");
-	private JCheckBox allTreesCheck = new JCheckBox("Save all trees");
-	private JCheckBox includeFigTreeCheck = new JCheckBox("Include FigTree block (NEXUS only)");
-	private JCheckBox includeAnnotationsCheck = new JCheckBox("Include Annotations (NEXUS only)");
+    private JComboBox formatCombo = null;
+    private JCheckBox asDisplayedCheck = new JCheckBox("Save as currently displayed");
+    private JCheckBox allTreesCheck = new JCheckBox("Save all trees");
+    private JCheckBox includeFigTreeCheck = new JCheckBox("Include FigTree block (NEXUS only)");
+    private JCheckBox includeAnnotationsCheck = new JCheckBox("Include Annotations (NEXUS & JSON only)");
 
-	public ExportTreeDialog(JFrame frame) {
+    public ExportTreeDialog(JFrame frame) {
 
-		OptionsPanel options = new OptionsPanel(12, 12);
+        OptionsPanel options = new OptionsPanel(12, 12);
 
-		formatCombo = new JComboBox(Format.values());
-		options.addComponentWithLabel("Tree file format: ", formatCombo);
+        formatCombo = new JComboBox(Format.values());
+        options.addComponentWithLabel("Tree file format: ", formatCombo);
 
-		options.addComponent(asDisplayedCheck);
-		options.addComponent(allTreesCheck);
-		options.addComponent(includeFigTreeCheck);
-		options.addComponent(includeAnnotationsCheck);
+        options.addComponent(asDisplayedCheck);
+        options.addComponent(allTreesCheck);
+        options.addComponent(includeFigTreeCheck);
+        options.addComponent(includeAnnotationsCheck);
 
-		optionPane = new JOptionPane(options,
-				JOptionPane.QUESTION_MESSAGE,
-				JOptionPane.OK_CANCEL_OPTION,
-				null,
-				null,
-				null);
-		optionPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+        optionPane = new JOptionPane(options,
+                JOptionPane.QUESTION_MESSAGE,
+                JOptionPane.OK_CANCEL_OPTION,
+                null,
+                null,
+                null);
+        optionPane.setBorder(new EmptyBorder(12, 12, 12, 12));
 
-		dialog = optionPane.createDialog(frame, "Export Trees");
-		dialog.pack();
+        dialog = optionPane.createDialog(frame, "Export Trees");
+        dialog.pack();
 
-		formatCombo.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				Object item = formatCombo.getSelectedItem();
+        formatCombo.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                Object item = formatCombo.getSelectedItem();
 
-				boolean isNexus = item.equals(Format.NEXUS);
-				includeFigTreeCheck.setEnabled(isNexus);
-				includeAnnotationsCheck.setEnabled(isNexus);;
-			}
-		});
-	}
+                includeFigTreeCheck.setEnabled(item.equals(Format.NEXUS));
+                includeAnnotationsCheck.setEnabled(item.equals(Format.NEXUS) || item.equals(Format.JSON));
+            }
+        });
+    }
 
-	public int showDialog() {
+    public int showDialog() {
 
 
-		dialog.setVisible(true);
+        dialog.setVisible(true);
 
-		int result = JOptionPane.CANCEL_OPTION;
-		Integer value = (Integer)optionPane.getValue();
-		if (value != null && value.intValue() != -1) {
-			result = value.intValue();
-		}
+        int result = JOptionPane.CANCEL_OPTION;
+        Integer value = (Integer)optionPane.getValue();
+        if (value != null && value.intValue() != -1) {
+            result = value.intValue();
+        }
 
-		if (result == JOptionPane.OK_OPTION) {
+        if (result == JOptionPane.OK_OPTION) {
 
-		}
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public Format getFormat() {
-		return (Format)formatCombo.getSelectedItem();
-	}
+    public Format getFormat() {
+        return (Format)formatCombo.getSelectedItem();
+    }
 
-	public boolean asDisplayed() {
-		return asDisplayedCheck.isSelected();
-	}
+    public boolean asDisplayed() {
+        return asDisplayedCheck.isSelected();
+    }
 
-	public boolean allTrees() {
-		return allTreesCheck.isSelected();
-	}
+    public boolean allTrees() {
+        return allTreesCheck.isSelected();
+    }
 
-	public boolean includeFigTreeBlock() {
-		return includeFigTreeCheck.isSelected();
-	}
+    public boolean includeFigTreeBlock() {
+        return includeFigTreeCheck.isSelected();
+    }
 
-	public boolean includeAnnotations() {
-		return includeAnnotationsCheck.isSelected();
-	}
+    public boolean includeAnnotations() {
+        return includeAnnotationsCheck.isSelected();
+    }
 
 
 }
