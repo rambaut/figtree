43d457cb8d244bfab27de78f5302fcb04e63a361
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2014-03-02 15:47:10 +0000

    Updating copyright and class comments throughout.

--- src/figtree/application/FigTreeApplication.java
+++ src/figtree/application/FigTreeApplication.java
@@ -25,7 +25,7 @@
  * Description:		An application for analysing MCMC trace files.
  * @author			Andrew Rambaut
  * @author			Alexei Drummond
- * @version			$Id: FigTreeApplication.java,v 1.15 2007/09/10 14:52:02 rambaut Exp $
+ * @version			$Id$
  */
 
 package figtree.application;
@@ -71,7 +71,7 @@ import ch.randelshofer.quaqua.QuaquaManager;
 public class FigTreeApplication extends MultiDocApplication {
 
     public static final String VERSION = "1.4.1";
-    public static final String DATES = "2006-2013";
+    public static final String DATES = "2006-2014";
 
     public static FigTreeApplication application;
 
