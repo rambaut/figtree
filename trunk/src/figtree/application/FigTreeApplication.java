304a606e004ddc53562c227c16d1b6d6605d9808
rambaut <rambaut@827c2e84-b43d-0410-b9f3-c512bdbbf1fa>
2015-03-05 11:24:23 +0000

    Updated packaging scripts to allow use of Java 7/8 on Mac.

--- trunk/src/figtree/application/FigTreeApplication.java
+++ trunk/src/figtree/application/FigTreeApplication.java
@@ -68,8 +68,8 @@ import ch.randelshofer.quaqua.QuaquaManager;
  */
 public class FigTreeApplication extends MultiDocApplication {
 
-    public static final String VERSION = "1.4.2";
-    public static final String DATES = "2006-2014";
+    public static final String VERSION = "1.4.3";
+    public static final String DATES = "2006-2015";
 
     public static FigTreeApplication application;
 
