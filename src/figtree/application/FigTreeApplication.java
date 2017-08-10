83474c2e30fc014dd1fe11a858b669ae03c354ba
Andrew Rambaut <rambaut@gmail.com>
2017-08-10 10:12:21 +0100

    Updating version

--- src/figtree/application/FigTreeApplication.java
+++ src/figtree/application/FigTreeApplication.java
@@ -76,8 +76,8 @@ import org.w3c.dom.DOMImplementation;
  */
 public class FigTreeApplication extends MultiDocApplication {
 
-    public static final String VERSION = "1.4.3";
-    public static final String DATES = "2006-2016";
+    public static final String VERSION = "1.4.4pre";
+    public static final String DATES = "2006-2017";
 
     public static FigTreeApplication application;
 
