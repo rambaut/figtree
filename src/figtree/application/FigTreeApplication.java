90057624ac1fe134372e82a7d883440d84b79f34
Andrew Rambaut <rambaut@gmail.com>
2025-08-21 12:01:43 +0100

    Updating version number towards v1.5.0

--- src/figtree/application/FigTreeApplication.java
+++ src/figtree/application/FigTreeApplication.java
@@ -66,8 +66,8 @@ import java.util.List;
  */
 public class FigTreeApplication extends MultiDocApplication {
 
-    public static final String VERSION = "1.4.5_pre";
-    public static final String DATES = "2006-2023";
+    public static final String VERSION = "1.5.0_pre";
+    public static final String DATES = "2006-2025";
 
     public static FigTreeApplication application;
 
